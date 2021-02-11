2095ef79b4ac5499d717117f35624aa3ea558f55
ilkkaturunen <iturunen@sonatype.com>
2021-02-11 11:43:00 +0000

    fix #1

--- repo-diff.py
+++ repo-diff.py
@@ -7,19 +7,19 @@ DEFAULT_HEADERS = {
     'User-Agent': 'iq-momo'}
 
 REPO_HOSTNAME= "http://localhost:8081"
-USER = 'LCpO0Lh8'
-TOKEN = 'aSVxh5KRrDJbZ04NGqRrEgQyzR1mNh_byFz-BWQhpS73'
+USER = 'username'
+TOKEN = 'password'
 
 REPOS = {
     # hosted: proxy
-    "python-hosted": "pypi-proxy",
-    "npm-hosted": "npm-group-proxy"
+    "ruby-hosted": "ruby-proxy",
+    "npm-hosted": "npm-proxy"
 }
 
 log = logging.getLogger('repo-diff')
 
 def main():
-    logging.basicConfig(filename='/Users/maurycupitt/sonatype/repo-diff/repo-diff.log', level=logging.INFO)
+    logging.basicConfig(filename='repo-diff.log', level=logging.INFO)
     for hosted, proxy in REPOS.items():
         print("-------------------")
         print("Hosted: {0} | Proxy: {1}".format(hosted, proxy))
@@ -31,20 +31,28 @@ def main():
 def get_artifacts(repo_name):
 
     component_list = []
+    continuation_token =""
 
-    response = requests.get('{0}/service/rest/v1/search/?sort=name&repository={1}'.format(
-       REPO_HOSTNAME, repo_name),
-       auth=(USER, TOKEN)
+    baseurl = (
+        f"{REPO_HOSTNAME}/service/rest/v1/search/?sort=name&repository={repo_name}"
     )
 
-    if response.ok:
-        res = json.loads(response.text)
-        # print("REPO NAME: {0} - {1}".format(repo_name, res))
-        for artifact in res['items']:
-            component = "{0}:{1}".format(artifact['group'], artifact['name'])
-            component_list.append(component)
-    return component_list
+    while continuation_token is not None:
+        url = baseurl
+        if continuation_token:
+            url += f"&continuationToken={continuation_token}"
+        response = requests.get(url, auth=(USER, TOKEN))
+        response.raise_for_status()
 
+        if response.ok:
+            res = json.loads(response.text)
+            for artifact in res['items']:
+                component = "{0}:{1}".format(artifact['group'], artifact['name'])
+                component_list.append(component)
+        
+        continuation_token = res["continuationToken"]
+    
+    return component_list
 
 if __name__ == '__main__':
     main()
